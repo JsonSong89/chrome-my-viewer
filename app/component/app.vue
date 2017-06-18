8970607daaba2433b1df90e0f1adc95dc4e8cbbe
jsonsong <sj178220709@163.com>
2017-06-19 00:16:00 +0800

    建立消息通道

--- app/component/app.vue
+++ app/component/app.vue
@@ -20,6 +20,9 @@
         name: 'vue@2.0'
       }
     },
+    mounted(){
+
+    },
     methods: {
       test1(){
         injectFileOnCurrentTab("scripts/vendors.js");
@@ -29,6 +32,25 @@
         injectFileOnCurrentTab('/scripts/initPage.js')
       },
       test3(){
+
+        chrome.tabs.query(
+          {active: true, currentWindow: true},
+          function (tabs) {
+            var port = chrome.tabs.connect(//建立通道
+              tabs[0].id,
+              {name: "ma"}//通道名称
+            );
+            $("#popup").click(function () {//给web页面的按钮绑定点击事件，通过点击事件来控制发送消息
+              port.postMessage({jia: "aaaaaaa"});//向通道中发送消息
+            });
+            port.onMessage.addListener(function (msg) {//这里同时利用通道建立监听消息，可以监听对方返回的消息
+              if (msg.jia == "yuuuuu") {//如果对方(popup.js)返回的消息是{jia: "yuuuuu"}则将扩展里面的input框的值设置为"yuuuuuuu"
+                $('#input').val("yuuuuuuu");
+              }
+              ;
+            });
+          });
+
       },
     },
     components: {
